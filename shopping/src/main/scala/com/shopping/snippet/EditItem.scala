002bdeea041649b41e2c5c5581334405762e333f
Oliver Daff <OliverDaff@hotmail.com>
2009-10-10 15:47:12 +0100

    added custom edit functionality

--- shopping/src/main/scala/com/shopping/snippet/EditItem.scala
+++ shopping/src/main/scala/com/shopping/snippet/EditItem.scala
@@ -1,12 +1,32 @@
-/*
- * EditItem.scala
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
-
 package com.shopping.snippet
 
+import com.shopping._
+import model._
+import net.liftweb._
+import http._
+
+import SHtml._
+import S._
+import mapper._
+import util._
+import Helpers._
+
+import scala.xml.{NodeSeq, Text}
+
 class EditItem {
+
+    var id = S.param("id") openOr ""
+
+    var item = try {
+        Item.findByKey(id.toLong)
+    } catch {
+        case e:NumberFormatException => Empty
+    }
+
+    def edit (html: NodeSeq): NodeSeq ={
+        item map ({ i =>
+            i.toForm(Full("save"), "/item/list")
+        }) openOr Text("Invalid Item")
+    }
 
 }
